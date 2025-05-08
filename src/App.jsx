e1806dbd0e9f44894d499cc089352e50afe317f9
Asen Chochev <asen.qkiqt145@gmail.com>
2025-05-08 22:27:27 +0300

    Create a structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className='min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-500'>
       
+      <div className="bg-white shadow-lg rounded-3xl p-16">
+        <h1 className='text-3xl font-bold text-center text-gray-900'>✅ React Taskify</h1>
+
+        <div className="mb-4 flex">
+          <input type="text" className="mt-5 border border-gray-300 rounded-l-lg p-2 w-full" placeholder="Add a new task..." />
+          <button className="mt-5 bg-blue-500 text-white rounded-r-lg px-4 py-2">Add</button>
+        </div>
+      </div>
     </div>
   )
 }
